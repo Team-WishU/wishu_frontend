ce7fdbddfc3b41f6788868b3e21f874a4773e47a
xevin03 <xevin03@naver.com>
2025-06-05 21:21:13 +0900

    초기 !

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,1 +1,33 @@
-export {}; 
+import React from "react";
+import logo from "../../assets/icons/logo.png";
+import searchIcon from "../../assets/icons/search.png";
+import "../../styles/Header.css";
+
+const Header: React.FC = () => {
+  return (
+    <header className="header">
+      <div className="header-inner">
+        {/* 왼쪽: 로고와 텍스트 */}
+        <div className="logo-container">
+          <img src={logo} alt="WishU Logo" />
+          <span className="logo-text">WishU</span>
+        </div>
+
+        {/* 가운데: 검색창 */}
+        <div className="search-container">
+          <input type="text" placeholder="아이템 검색" />
+          <img src={searchIcon} alt="Search Icon" />
+        </div>
+
+        {/* 오른쪽: 프로필 */}
+        <div className="profile-container">
+          <div className="profile-circle" />
+          <span>예콩</span>
+          <span>▾</span>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;

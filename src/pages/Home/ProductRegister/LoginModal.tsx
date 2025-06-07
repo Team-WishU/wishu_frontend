263b81f3546290f9e42f5ab6d3e8d07b217d96d0
ChoiSuJin03 <106504517+ChoiSuJin03@users.noreply.github.com>
2025-06-07 13:20:06 +0900

    로그인 모달

--- src/pages/Home/ProductRegister/LoginModal.tsx
+++ src/pages/Home/ProductRegister/LoginModal.tsx
@@ -1,7 +1,7 @@
-// src/pages/Home/ProductRegister/LoginModal.tsx
 import React from "react";
 import "../../../styles/ProductRegister/LoginModal.css";
-import logo from "../../../assets/icons/logo.png";
+
+const logo = process.env.PUBLIC_URL + "/assets/icons/logo.png";
 
 interface LoginModalProps {
   isOpen: boolean;
@@ -12,35 +12,38 @@ const LoginModal: React.FC<LoginModalProps> = ({ isOpen, onClose }) => {
   if (!isOpen) return null;
 
   return (
-    <div className="login-modal-overlay">
-      <div className="login-modal">
+    <div className="modal-overlay">
+      <div className="modal-box">
         <button className="modal-close" onClick={onClose}>
           ✕
         </button>
 
-        <div className="modal-header-with-logo">
-          <div className="logo-circle">
-            <img src={logo} alt="Wishu Logo" className="logo-image" />
+        <div className="modal-header">
+          <div className="logo-wrapper">
+            <img src={logo} alt="Wishu Logo" className="logo-img" />
           </div>
-          <div className="text-block">
+          <div className="modal-text">
             <h2>WELCOME!</h2>
             <p>위시랩에 로그인하고, 아이템을 구경해보세요</p>
           </div>
         </div>
-        <input
-          type="email"
-          placeholder="이메일(아이디)를 입력해주세요."
-          className="modal-input"
-        />
-        <input
-          type="password"
-          placeholder="비밀번호를 입력해주세요."
-          className="modal-input"
-        />
-        <button className="login-button">로그인</button>
-        <p className="signup-text">
-          계정이 없으세요? <span className="signup-link">가입하기</span>
-        </p>
+
+        <div className="modal-content">
+          <input
+            type="email"
+            placeholder="이메일(아이디)를 입력해주세요."
+            className="modal-input"
+          />
+          <input
+            type="password"
+            placeholder="비밀번호를 입력해주세요."
+            className="modal-input"
+          />
+          <button className="login-btn">로그인</button>
+          <p className="signup-msg">
+            계정이 없으세요? <span className="signup-link">가입하기</span>
+          </p>
+        </div>
       </div>
     </div>
   );
